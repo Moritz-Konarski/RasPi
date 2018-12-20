c1d4a884edaf36b02fa2d9f36694238234b443d3
Moritz Konarski <konarski_m@auca.kg>
2018-12-21 01:28:14 +0600

    initialized the files

---
+++ Temperature Measurement Station/Temp Measure Code/Main.py
@@ -0,0 +1,12 @@
+import LCD
+import Sensor
+import GetInfo
+
+# the sensors as subclasses
+Temperature = Sensor.Temperature
+Humidity = Sensor.Humidity
+Light = Sensor.Light
+# the lcd as a class
+LcDisplay = LCD.LcDisplay
+# the getting information funtion as a class
+GetInfo = GetInfo.GetInfo
---
+++ Temperature Measurement Station/Temp Measure Code/Sensor.py
@@ -0,0 +1,11 @@
+class Sensor:
+    pass
+
+class Temperature(Sensor):
+    pass
+
+class Humidity(Sensor):
+    pass
+
+class Light(Sensor):
+    pass
